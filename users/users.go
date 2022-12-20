670a890705a0a480294c02a13bd7d6696f7632e7
rizal-fahmi <rizalfahmi3002@gmail.com>
2022-12-20 11:21:33 +0700

    function register pegawai baru

--- users/users.go
+++ users/users.go
@@ -1,1 +1,61 @@
 package users
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
+type Pegawai struct {
+	ID				int
+	Username	string
+	Password	string
+}
+
+type AuthMenu struct {
+	DB *sql.DB
+}
+
+// Function untuk menambah pegawai baru
+func (am *AuthMenu) Register(tambahPegawai Pegawai) (bool, error) {
+	registerQry, err := am.DB.Prepare("INSERT INTO pegawai (username, password) values (?,?)")
+	if err != nil {
+		log.Println("Prepare insert pegawai ", err.Error())
+		return false, errors.New("")
+	}
+
+	if am.Duplicate(tambahPegawai.Username) {
+		log.Println("duplicated information")
+		return false, errors.New("name sudah digunakan")
+	}
+
+	res, err := registerQry.Exec(tambahPegawai.Username, tambahPegawai.Password)
+	if err != nil {
+		log.Println("insert pegawai ", err.Error())
+		return false, errors.New("insert pegawai error")
+	}
+
+	affRows, err := res.RowsAffected()
+
+	if err != nil {
+		log.Println("After insert pegawai ", err.Error())
+		return false, errors.New("Error setelah insert pegawai")
+	}
+
+	if affRows <= 0 {
+		log.Println("No record affected")
+		return false, errors.New("No record")
+	}
+	return true, nil
+}
+
+func (am *AuthMenu) Duplicate(username string) bool {
+	res := am.DB.QueryRow("SELECT id FROM pegawai where username = ?", username)
+	var idExist int
+	err := res.Scan(&idExist)
+	if err != nil {
+		log.Println("Result scan error", err.Error())
+		return false
+	}
+	return true
+}
