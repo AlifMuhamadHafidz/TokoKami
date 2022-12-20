e4fbb507528cd7309cfa3cf2f97e0fd5173a120a
Alif Muhamad Hafidz <alifmuhamadhafidz23@gmail.com>
2022-12-20 11:10:15 +0700

    feature login

--- users/users.go
+++ users/users.go
@@ -1,1 +1,43 @@
 package users
+
+import (
+	"database/sql"
+	"errors"
+	"log"
+)
+
+type Pegawai struct {
+	ID       int
+	Nama     string
+	Password string
+}
+
+type AuthMenu struct {
+	DB *sql.DB
+}
+
+func (am *AuthMenu) Login(nama string, password string) (Pegawai, error) {
+	loginQry, err := am.DB.Prepare("SELECT id FROM users WHERE username = ? AND password = ?")
+	if err != nil {
+		log.Println("prepare insert user ", err.Error())
+		return Pegawai{}, errors.New("prepare statement insert user error")
+	}
+
+	row := loginQry.QueryRow(nama, password)
+
+	if row.Err() != nil {
+		log.Println("login query ", row.Err().Error())
+		return Pegawai{}, errors.New("tidak bisa login, data tidak ditemukan")
+	}
+	res := Pegawai{}
+	err = row.Scan(&res.ID)
+
+	if err != nil {
+		log.Println("after login query ", err.Error())
+		return Pegawai{}, errors.New("tidak bisa login, kesalahan setelah error")
+	}
+
+	res.Nama = nama
+
+	return res, nil
+}
